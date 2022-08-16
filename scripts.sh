ed93a7d007835e5fc89977011dc26b324549366e
alangithub2018 <43253925+alangithub2018@users.noreply.github.com>
2022-08-15 21:53:14 -0500

    Setup elasticsearch and kibana via shell commands

--- scripts.sh
+++ scripts.sh
@@ -46,3 +46,23 @@ sudo chmod +x /usr/local/bin/ctop
 
 ####################################Install ctop#########################
 #########################################################################
+
+#################Install elasticsearch  #################################
+#########################################################################
+
+sysctl -w vm.max_map_count=262144
+docker network create elastic
+docker pull docker.elastic.co/elasticsearch/elasticsearch:8.3.3
+docker run --name es-node01 --net elastic -p 9200:9200 -p 9300:9300 -t docker.elastic.co/elasticsearch/elasticsearch:8.3.3
+
+#################Install elasticsearch  #################################
+#########################################################################
+
+# take a break here and open another terminal because you`ll need 
+# enrollment token for kibana here
+
+#################Install Kibana  ########################################
+#########################################################################
+
+docker pull docker.elastic.co/kibana/kibana:8.3.3
+docker run --name kib-01 --net elastic -p 5601:5601 docker.elastic.co/kibana/kibana:8.3.3
