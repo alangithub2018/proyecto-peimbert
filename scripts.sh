6b61ef78f28952176f044c5d537432ca0833f916
alangithub2018 <43253925+alangithub2018@users.noreply.github.com>
2022-08-15 21:03:37 -0500

    Update script using comments

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,6 @@
+###########################Install docker##########################
+###################################################################
+
 sudo apt-get update \ install \
     ca-certificates \
     curl \
@@ -20,3 +23,26 @@ sudo apt-get update
  $ sudo apt-get install docker-ce docker-ce-cli containerd.io 
 docker-compose-plugin
 
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://azlux.fr/repo.gpg.key | sudo gpg --dearmor -o 
+/usr/share/keyrings/azlux-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) 
+signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] 
+http://packages.azlux.fr/debian \
+  $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azlux.list 
+>/dev/null
+sudo apt-get update
+sudo apt-get install docker-ctop
+
+####################################Install docker#######################
+#########################################################################
+
+####################################Install ctop#########################
+#########################################################################
+
+sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.1/ctop-0.7.1-linux-amd64  -O /usr/local/bin/ctop
+sudo chmod +x /usr/local/bin/ctop
+
+####################################Install ctop#########################
+#########################################################################
