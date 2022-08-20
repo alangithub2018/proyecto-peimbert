312297d1cd5302e7a3cdad6fbe0f949bcdc05732
alangithub2018 <43253925+alangithub2018@users.noreply.github.com>
2022-08-20 12:04:34 -0500

    Add terraform to our project

---
+++ Terraform/.bash_profile
@@ -0,0 +1,1 @@
+export GOOGLE_APPLICATION_CREDENTIALS = /Users/macbookair/Documents/GitHub/proyecto-peimbert/Terraform/stps-360015-53db0888d7d1.json
---
+++ Terraform/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "stps-360015"
+  region  = "us-west1"
+  zone    = "us-west1-a"
+}
+
+resource "google_compute_instance" "vm_stps_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
