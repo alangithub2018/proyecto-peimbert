54e6c771d13b8b2b06c35e85bd678d70f040da59
alangithub2018 <43253925+alangithub2018@users.noreply.github.com>
2022-08-24 03:57:34 -0500

    Add customized script

--- Terraform/data/init.sh
+++ Terraform/data/init.sh
@@ -1,3 +1,10 @@
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker.io 
+sudo apt-get install docker-compose
+sudo apt install git -y
 cd /
 rm -R /proyecto/
 sudo mkdir -p /proyecto/peimbert
@@ -5,16 +12,15 @@ cd /proyecto/peimbert
 git clone https://github.com/alangithub2018/proyecto-peimbert.git
 cd /proyecto/peimbert/proyecto-peimbert/Final_Image
 chmod 777 -R /proyecto/
+
 docker-compose up -d
 cd /proyecto/peimbert/proyecto-peimbert/Final_Image/MongoCluster/
 sleep 5
-chmod a+x rs-init.sh 
+chmod a+x rs-init.sh 
 docker exec mongo1 /scripts/rs-init.sh
 cd /proyecto/peimbert/proyecto-peimbert/Final_Image
 docker-compose -f /proyecto/peimbert/proyecto-peimbert/Final_Image/docker-compose-api.yml up -d
 chmod a+x /proyecto/peimbert/proyecto-peimbert/Final_Image/Nginx/init-letsencrypt.sh
 ./proyecto/peimbert/proyecto-peimbert/Final_Image/Nginx/init-letsencrypt.sh
-cd /proyecto/peimbert/proyecto-peimbert/Final_Image/Nginx
-./init-letsencrypt.sh
 cd /proyecto/peimbert/proyecto-peimbert/Final_Image/Nginx
 ./init-letsencrypt.sh
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,25 +1,63 @@
 provider "google" {
   project = "stps-360015"
-  region  = "us-west1"
-  zone    = "us-west1-a"
+  region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
 resource "google_compute_instance" "vm_stps_instance" {
   name         = "terraform-instance"
-  machine_type = "e2-micro"
+  machine_type = "e2-medium"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
+
+  /*connection {
+    type        = "ssh"
+    user        = "mtywdm"
+    timeout     = "500s"
+    private_key = file(var.privatekeypath)
+    host        = "34.135.246.3"
+  }*/
+
+  /*provisioner "file" {
+    source = "data/script.sh"
+    destination = "/tmp/init.sh"
+  }
+
+   provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/init.sh",
+      "/tmp/init.sh"
+    ]
+  }*/
 
   network_interface {
     # A default network is created for all GCP projects
     network = google_compute_network.vpc_network.self_link
     access_config {
+      nat_ip = "34.135.246.3"
     }
   }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22-27023"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  source_tags = ["web"]
 }
 
 resource "google_compute_network" "vpc_network" {
